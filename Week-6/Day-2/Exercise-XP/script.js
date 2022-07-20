271cef7253da62bec290db1946f5e1c24d2a033f
samp101 <shmpos@gmail.com>
2022-07-20 20:14:36 +0300

    Just Exercise-XP

--- Week-6/Day-2/Exercise-XP/script.js
+++ Week-6/Day-2/Exercise-XP/script.js
@@ -112,47 +112,7 @@ let bankAmount = 0
 let tax = .17
 let expenses = [200, -100, 146, 167, -2900]
 
-expenses.forEach((element,ind) => {expenses[ind] = element*1.17})
-bankAmount = expenses.reduce((first,last ) => first +last, );
+expenses.forEach((element,ind,arr) => expenses[ind] > 0 ? arr[ind] = element - (element*.17):element)
+
+bankAmount = expenses.reduce((first,last ) => first +last);
 console.log(bankAmount)
-
-winner = []
-score = 0
-const gameInfo = [
-    {
-      username: "john",
-      team: "red",
-      score: 5,
-      items: ["ball", "book", "pen"]
-    },
-    {
-      username: "becky",
-      team: "blue",
-      score: 10,
-      items: ["tape", "backpack", "pen"]
-    },
-    {
-      username: "susy",
-      team: "red",
-      score: 55,
-      items: ["ball", "eraser", "pen"]
-    },
-    {
-      username: "tyson",
-      team: "green",
-      score: 1,
-      items: ["book", "pen"]
-    },
-   ];
-
-
-gameInfo.forEach(element => {
-    element['username'] = `${element['username']}!`
-    console.log(element['username']); 
-});
-gameInfo.forEach(function(a) {if(a.score > 5) {winner.push(a.username)}})
-gameInfo.forEach(a => score += a.score)
-
-console.log(winner);
-console.log(score);
-
