aa2d01b15e1315dddd1c4bbaa3135b006ce54afc
samp101 <shmpos@gmail.com>
2022-07-25 14:39:08 +0300

    W6D4 Exercise XP

---
+++ Week-6/Day-4/Exercise-Gold/script.js
@@ -0,0 +1,28 @@
+let landscape = function() {
+
+    let result = "";
+   
+    let flat = (x)=>{
+      for(let count = 0; count<x; count++){
+        result = result + "_";
+      }
+    }
+   
+    let mountain = (x) => {
+      result = result + "/"
+      for(let counter = 0; counter<x; counter++){
+        result = result + "'"
+      }
+      result = result + "\\"
+      alert('hjew')
+    }
+   
+    flat(4);
+    mountain(4);
+    flat(4)
+   
+    return result;
+   }
+   
+   console.log(landscape())
+//    ____/''''\____
