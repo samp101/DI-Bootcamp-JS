7008a32c22a85fbfae258ed0a408aff1cbc08f55
samp101 <shmpos@gmail.com>
2022-07-25 14:42:26 +0300

    recommit

--- Week-6/Day-4/Exercise-Gold/script.js
+++ Week-6/Day-4/Exercise-Gold/script.js
@@ -1,20 +1,19 @@
 let landscape = function() {
 
     let result = "";
-   
+//    Here I changed it to an an arrow function is this a nested afunction whihc they are asking for?
     let flat = (x)=>{
       for(let count = 0; count<x; count++){
         result = result + "_";
       }
     }
-   
+//    Here I changed it to an an arrow function is this a nested afunction whihc they are asking for?
     let mountain = (x) => {
       result = result + "/"
       for(let counter = 0; counter<x; counter++){
         result = result + "'"
       }
       result = result + "\\"
-      alert('hjew')
     }
    
     flat(4);
@@ -26,3 +25,4 @@ let landscape = function() {
    
    console.log(landscape())
 //    ____/''''\____
+let div;
