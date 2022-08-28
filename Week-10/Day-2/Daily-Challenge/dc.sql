9bdf13054336c11cd5a84b61476722f5df733e52
samp101 <shmpos@gmail.com>
2022-08-28 21:18:46 +0300

    W10D2 Daily-Challenge

---
+++ Week-10/Day-2/Daily-Challenge/dc.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(actor_id)
+from actor;
+
+-- select * from actor;
+-- insert into actor (first_name,last_name,last_update)
+-- values 
+-- 	('shmuel','posner')
+-- -- 	wont work because it has to parameter than the actualvalues that your providing
+	
+	
+	
